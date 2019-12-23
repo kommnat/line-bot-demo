cf21c61bd02936ef4c65278947651abdede5412a
MoshiNAT <kommanat01@gmail.com>
2019-12-23 23:45:05 +0700

    test send message3

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/webhook', (req, res) => {
     let msg = req.body.events[0].message.text
     if(msg == 'chelsea' || msg == 'Chelsea' ){
         reply(reply_token, msg)
-    }else{
+    }else if(msg == 'list' || msg == 'list'){
         reply1(reply_token, msg)
     }
     
@@ -63,14 +63,171 @@ function reply1(reply_token ,msg) {
     let body = JSON.stringify({
         replyToken: reply_token,
        
-        messages: [{
-            type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: msg
-        }]
+        messages: [ {
+            "type": "flex",
+            "altText": "Flex Message",
+            "contents": {
+              "type": "bubble",
+              "direction": "ltr",
+              "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "สถานะ",
+                        "flex": 2,
+                        "size": "lg",
+                        "weight": "bold",
+                        "color": "#AAAAAA"
+                      },
+                      {
+                        "type": "text",
+                        "text": "รอดำเนินการ",
+                        "flex": 5,
+                        "size": "lg",
+                        "weight": "bold",
+                        "color": "#666666",
+                        "wrap": true
+                      }
+                    ]
+                  }
+                ]
+              },
+              "hero": {
+                "type": "image",
+                "url": "https://news.mthai.com/app/uploads/2019/09/cropped-%E0%B8%A3%E0%B8%93%E0%B8%A3%E0%B8%87%E0%B8%84%E0%B9%8C%E0%B9%80%E0%B8%81%E0%B9%87%E0%B8%9A%E0%B8%82%E0%B8%A2%E0%B8%B0_190921_0010.jpg",
+                "size": "full",
+                "aspectRatio": "1.51:1",
+                "aspectMode": "fit"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "margin": "lg",
+                    "contents": [
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "ประเภท",
+                            "flex": 2,
+                            "size": "lg",
+                            "weight": "bold",
+                            "color": "#AAAAAA",
+                            "action": {
+                              "type": "message",
+                              "label": "เปลี่ยนประเภท",
+                              "text": "เปลี่ยนประเภทของ ขยะนอกถังมาเก็บด้วย"
+                            }
+                          },
+                          {
+                            "type": "text",
+                            "text": "ขยะ",
+                            "flex": 5,
+                            "size": "lg",
+                            "weight": "bold",
+                            "color": "#666666",
+                            "wrap": true
+                          }
+                        ]
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "คำอธิบาย",
+                            "flex": 2,
+                            "size": "sm",
+                            "color": "#AAAAAA"
+                          },
+                          {
+                            "type": "text",
+                            "text": "ขยะนอกถังมาเก็บด้วย",
+                            "flex": 5,
+                            "size": "sm",
+                            "color": "#666666",
+                            "wrap": true
+                          }
+                        ]
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "ตำแหน่ง",
+                            "flex": 2,
+                            "size": "sm",
+                            "color": "#AAAAAA"
+                          },
+                          {
+                            "type": "text",
+                            "text": "[ตำแหน่งที่ถูกรายงาน]",
+                            "flex": 5,
+                            "size": "sm",
+                            "color": "#666666",
+                            "wrap": true
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "message",
+                      "label": "กำลังดำเนินการ",
+                      "text": "กำลังดำเนินการ ขยะนอกถังมาเก็บด้วย"
+                    },
+                    "color": "#805637",
+                    "margin": "lg",
+                    "style": "primary"
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "message",
+                      "label": "เสร็จสิ้น",
+                      "text": "ดำเนินการเรื่อง ขยะนอกถังมาเก็บด้วย เสร็จแล้ว"
+                    },
+                    "color": "#805637",
+                    "margin": "sm",
+                    "style": "primary"
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "message",
+                      "label": "ไม่เกี่ยวข้อง",
+                      "text": "ดำเนินการเปลี่ยน ขยะนอกถังมาเก็บด้วย เป้นไม่เกี่ยวข้อง"
+                    },
+                    "color": "#805637",
+                    "margin": "sm",
+                    "style": "primary"
+                  }
+                ]
+              }
+            }
+          }]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
