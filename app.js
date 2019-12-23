49d5a3068c86410b1290bda2e72f49a26ba4e6df
MoshiNAT <kommanat01@gmail.com>
2019-12-23 10:46:49 +0700

    test send message10

--- app.js
+++ app.js
@@ -1,160 +1,70 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
+// Reply with two static messages
 
-require('dotenv').config();
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-const app = express();
+app.get('/bot3', (req, res) => {
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    res.sendStatus(200) 
+    // console.log('Hello World');
+})
 
-const config = {
-    channelAccessToken: process.env.channelAccessToken,
-    channelSecret: process.env.channelSecret
-};
+app.post('/webhook', (req, res) => {
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    
+    res.sendStatus(200)
+})
+app.listen(port)
 
-const client = new line.Client(config);
-
-app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
-});
-
-
-function handleEvent(event) {
-
-    console.log(event);
-    if (event.type === 'message' && event.message.type === 'text') {
-        handleMessageEvent(event);
-    } else {
-        return Promise.resolve(null);
-    }
-}
-function reply(reply_token, msg) {
+function reply(reply_token, msg, img) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU=}'
     }
-
-    var msg = {
-        type: 'text',
-        text: 'สวัสดีครัช'
-    };
-    var eventText = event.message.text.toLowerCase();
-    if (eventText === 'image') {
-        msg = {
-            'type': 'image',
-            'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
-            'previewImageUrl': 'https://images.performgroup.com/di/library/GOAL/a6/bb/fifa-18-ronaldo_lx3r88bpjpk91re36ukdgomrj.jpg?t=2027563652&w=620&h=430'
-        }
-    } else if (eventText === 'location') {
-        msg = {
-            "type": "location",
-            "title": "my location",
-            "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-            "latitude": 35.65910807942215,
-            "longitude": 139.70372892916203
-        }
-    } else if (eventText === 'template button') {
-        msg = {
-            "type": "template",
-            "altText": "this is a buttons template",
-            "template": {
-                "type": "buttons",
-                "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                "title": "Menu",
-                "text": "Please select",
-                "actions": [{
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=123"
-                }, {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=123"
-                }, {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/123"
-                }]
-            }
-        }
-    } else if (eventText === 'template confirm') {
-        msg = {
-            "type": "template",
-            "altText": "this is a confirm template",
-            "template": {
-                "type": "confirm",
-                "text": "Are you sure?",
-                "actions": [{
-                    "type": "message",
-                    "label": "Yes",
-                    "text": "yes"
-                }, {
-                    "type": "message",
-                    "label": "No",
-                    "text": "no"
-                }]
-            }
-        }
-    } else if (eventText === 'carousel') {
-        msg = {
-            "type": "template",
-            "altText": "this is a carousel template",
-            "template": {
-                "type": "carousel",
-                "columns": [
-                    {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "this is menu",
-                        "text": "description",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=111"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=111"
-                            },
-                            {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/111"
-                            }
-                        ]
-                    },
-                    {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "this is menu",
-                        "text": "description",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=222"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=222"
-                            },
-                            {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/222"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
-
-    return client.replyMessage(event.replyToken, msg);
- 
-   }   app.set('port', (process.env.PORT || 5000));
-
-    app.listen(app.get('port'), function () {
-        console.log('run at port', app.get('port'));
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello , ',
+        },
+        {
+            type: 'text',
+            text: msg
+        },
+        {
+            type: 'text',
+            text: reply_token
+        },
+        {
+            type: "image",
+            url: "https://news.mthai.com/app/uploads/2019/09/cropped-%E0%B8%A3%E0%B8%93%E0%B8%A3%E0%B8%87%E0%B8%84%E0%B9%8C%E0%B9%80%E0%B8%81%E0%B9%87%E0%B8%9A%E0%B8%82%E0%B8%A2%E0%B8%B0_190921_0010.jpg",
+            size: "full",
+            aspectRatio: "1.51:1",
+            aspectMode: "fit"
+        },
+        ]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
     });
+}
