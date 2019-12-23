bdaf31da82257c88f9d03bd996fa1541868123ce
MoshiNAT <kommanat01@gmail.com>
2019-12-23 15:35:34 +0700

    test send message10

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ app.get('/bot3', (req, res) => {
     res.header("Cache-Control", "no-cache, no-store, must-revalidate");
     res.header("Pragma", "no-cache");
     res.header("Expires", 0);
-    let reply_token = req.body.events[0].replyToken
-    let msg = req.body.events[0].message.text
-    reply(reply_token, msg)
     res.sendStatus(200) 
     // console.log('Hello World');
 })
@@ -26,40 +23,25 @@ app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
     reply(reply_token, msg)
-    
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token, msg) {
+function reply(reply_token ,msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU=}'
     }
-    if(msg == 'kommanat'){
-        let body = JSON.stringify({
-            replyToken: reply_token,
-            messages: [{
-                type: 'text',
-                text: 'Hello , ',
-            },
-            {
-                type: 'text',
-                text: msg
-            }]
-        })
-    }else{
-        let body = JSON.stringify({
-            replyToken: reply_token,
-            messages: [
-            {
-                type: 'text',
-                text: 'สวัสดี '
-            }]
-        })
-    }
-    
-
-    
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: msg
+        }]
+    })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
