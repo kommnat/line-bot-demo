2a67b3d206a86f24a5abdbfb569d3639ae5445c5
MoshiNAT <kommanat01@gmail.com>
2019-12-23 16:03:48 +0700

    test send message10

--- app.js
+++ app.js
@@ -26,32 +26,53 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token ,msg) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU=}'
+if(req.body.events[0].message.text == 'chelsea'){
+    function reply(reply_token ,msg) {
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU=}'
+        }
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                'type': 'image',
+                'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
+                'previewImageUrl': 'https://upload.wikimedia.org/wikipedia/th/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png'
+            },]
+          
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
     }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            'type': 'image',
-            'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
-            'previewImageUrl': 'https://upload.wikimedia.org/wikipedia/th/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png'
-        },]
-        // messages: [{
-        //     type: 'text',
-        //     text: 'Hello'
-        // },
-        // {
-        //     type: 'text',
-        //     text: msg
-        // }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
+}else{
+    function reply(reply_token ,msg) {
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU=}'
+        }
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            // messages: [{
+            //     'type': 'image',
+            //     'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
+            //     'previewImageUrl': 'https://upload.wikimedia.org/wikipedia/th/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png'
+            // },]
+            messages: [{
+                type: 'text',
+                text: 'Hello'
+            },]
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
+    }
 }
