db122d25487a55bf10f796aafccfa3a8da8b76b4
MoshiNAT <kommanat01@gmail.com>
2019-12-23 22:33:08 +0700

    test send message2

--- app.js
+++ app.js
@@ -20,9 +20,15 @@ app.post('/webhook', (req, res) => {
     res.header("Cache-Control", "no-cache, no-store, must-revalidate");
     res.header("Pragma", "no-cache");
     res.header("Expires", 0);
+
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    reply(reply_token, msg)
+    if(msg == 'chelsea' || msg == 'Chelsea' ){
+        reply(reply_token, msg)
+    }else{
+        reply1(reply_token, msg)
+    }
+    
     res.sendStatus(200)
 })
 app.listen(port)
@@ -31,8 +37,7 @@ function reply(reply_token ,msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU=}'
-    }
-   
+    }  
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
@@ -40,14 +45,32 @@ function reply(reply_token ,msg) {
             'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
             'previewImageUrl': 'https://upload.wikimedia.org/wikipedia/th/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png'
         },]
-        // messages: [{
-        //     type: 'text',
-        //     text: 'Hello'
-        // },
-        // {
-        //     type: 'text',
-        //     text: msg
-        // }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+
+function reply1(reply_token ,msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU=}'
+    }  
+    let body = JSON.stringify({
+        replyToken: reply_token,
+       
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: msg
+        }]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
