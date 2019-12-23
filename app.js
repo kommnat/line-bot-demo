ca7f1c06d6da49ce4990e63f36c039e7a2c2abe5
MoshiNAT <kommanat01@gmail.com>
2019-12-23 16:56:13 +0700

    test send message10

--- app.js
+++ app.js
@@ -22,49 +22,34 @@ app.post('/webhook', (req, res) => {
     res.header("Expires", 0);
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    reply(reply_token, msg)
+    let img = req.body.events[0]
+    reply(reply_token, msg ,img)
     res.sendStatus(200)
 })
 app.listen(port)
 
-function reply(reply_token ,msg) {
+function reply(reply_token ,msg ,img) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {tXOxbBNHDvyxUTITtlUkErraVe0AtpyFeb8Shb3+33rcl826FIjNK6eoMvgfU/6Fnmj1h9nNG6Km+ZeN6YG9BFg5phdOAhZscsvKT23QR8i6lr4f112jGMLQqLG/1mwQrQCJANMtk/SqfnhPjiy2gAdB04t89/1O/w1cDnyilFU=}'
     }
-    if( msg == 'chelsea'){
-        
-        let body = JSON.stringify({
-            replyToken: reply_token,
-            messages: [{
-                'type': 'image',
-                'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
-                'previewImageUrl': 'https://upload.wikimedia.org/wikipedia/th/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png'
-            },]
-            // messages: [{
-            //     type: 'text',
-            //     text: 'Hello'
-            // },
-            // {
-            //     type: 'text',
-            //     text: msg
-            // }]
-        })
-    }else{
-        let body = JSON.stringify({
-            replyToken: reply_token,
-           
-            messages: [{
-                type: 'text',
-                text: 'Hello'
-            },
-            {
-                type: 'text',
-                text: msg
-            }]
-        })
-    }
-    
+   
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        // messages: [{
+        //     'type': 'image',
+        //     'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
+        //     'previewImageUrl': 'https://upload.wikimedia.org/wikipedia/th/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png'
+        // },]
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: img
+        }]
+    })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
